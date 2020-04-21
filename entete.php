d1221d8787697b68f4e76b09b1fbd0ca16b5648a
CyrielleGl <cyrielle.gallou@gmail.com>
2020-04-21 14:40:54 +0200

    titre + lien tel

--- entete.php
+++ entete.php
@@ -5,14 +5,14 @@
 
 <header>
 
-<a href="index.php"><h1>Développeuse Web & Webmarketing</h1></a>
+<a href="index.php"><h1>Développeuse Web & Design</h1></a>
 
     <div id="center_block"><p class="false-search-bar">🔎 <u>Contrat d'apprentissage - 2/3 ans</u></p></div>
 
     <div id="slide">      
                 <div id="content-slide">
                     
-                    <p><img src="./phone.png" id="picto"/> 06.21.49.29.11</p>
+                    <p><img src="./phone.png" id="picto"/> <a href="tel:+33621492911" class="link">06.21.49.29.11</a></p>
                     <p><img src="./mail.png" id="picto"/> <a href="mailto:cyrielle.gallou@gmail.com" class="link">cyrielle.gallou@gmail.com</a></p>
 
                 </div>
