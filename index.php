f00c249763d1e51706370dae536635bea9b57df2
CyrielleGl <cyrielle.gallou@gmail.com>
2020-07-05 14:13:53 +0200

    texte introduction

--- index.php
+++ index.php
@@ -25,8 +25,10 @@
         <p>
             Acceptée à l'école <b>IPSSI</b> pour le <strong>Master "Architecture Web & Big Data"</strong>, <br/>je suis actuellement à la recherche d'une alternance en contrat d'apprentissage pour 2 ou 3 ans. <br />
             <br/>Je cherche une entreprise qui pourra m'apporter autant que je souhaite lui apporter en termes de compétences. <br/><br/>
-            Après l'obtention d'un Master en communication et la validation de mes compétences lors de mes expériences qui en découlent, <br/><strong>je souhaite aujourd'hui me diriger vers un métier 100% IT.</strong><br/><br/>Attirée depuis toujours par les nouvelles technologies, j'ai découvert mon appétence pour le métier de développeur <br/>lors de mes expériences en communication (avec notamment l'utilisation du HTML5 & CSS3), <br/>mais aussi au fil de mes <b>rencontres professionnelles qui ont pu m'apporter une vision plus précise de ce métier.</b><br/><br/>
-            Je vous invite donc à <b>naviguer dans le menu</b> <br/>afin de découvrir mes formations, mes expériences et mes différentes compétences, <br/>et à <strong>prendre contact avec moi</strong> si mon profil vous intéresse !<br/>
+            Après l'obtention d'un Master en communication et la validation de mes compétences lors de mes expériences qui en découlent, <strong>je souhaite aujourd'hui me diriger vers un métier 100% IT.</strong><br/><br/>
+            Ayant une <b>forte appétence pour Angular</b>, je me forme sur cette technologie à travers divers projets, dont vous pouvez consulter l'avancement sur mon Git : <br/>
+            <br/><b><a href="https://github.com/CyrielleGl" target="_blank">Mon GitHub</a></b>
+            <br/><br/>Je vous invite à <b>naviguer dans le menu</b> <br/>afin de découvrir mes formations, mes expériences et mes différentes compétences, <br/>et à <strong>prendre contact avec moi</strong> si mon profil vous intéresse !<br/>
             
             <br/>Bonne visite !<br/>
         </p>
